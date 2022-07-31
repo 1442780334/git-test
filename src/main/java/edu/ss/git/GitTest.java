f08c4e73d50864abd3394cd5a914cd36aeebb0f0
zhuimeng <1442780334@qq.com>
2022-07-31 19:10:21 +0800

    first commit

---
+++ src/main/java/edu/ss/git/GitTest.java
@@ -0,0 +1,12 @@
+package edu.ss.git;
+
+/**
+ * @author gxz
+ * @version 1.0
+ * @date 2022/7/31 7:06 下午
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
